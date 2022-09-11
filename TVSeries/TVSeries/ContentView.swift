bd9a2ae7d9283e2df4215f54b0e0e39e5ec568b9
Douglas Garcia <douglas.garcia@ymail.com>
2022-09-11 19:06:16 -0300

    Init Project

---
+++ TVSeries/TVSeries/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TVSeries
+//
+//  Created by Douglas Garcia on 10/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TVSeries/TVSeries/TVSeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVSeriesApp.swift
+//  TVSeries
+//
+//  Created by Douglas Garcia on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TVSeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
